a70a9f4a846ee6e6059a7bd1832073ecb070df11
mamun-prodhan <mamunpr9@gmail.com>
2023-11-03 00:34:42 +0600

    hook modified

--- src/hooks/useServices.jsx
+++ src/hooks/useServices.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-const useServices = (url) => {
+const useServices = () => {
   const [services, setServices] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [url]);
+  }, []);
   return services;
 };
 
